0b95363857f209bef3853f818c82a84f3006b996
darkwire37 <71898092+darkwire37@users.noreply.github.com>
2022-04-07 13:30:34 -0400

    added a reminder to enable forwarding

--- scripts/sparky/addNat.sh
+++ scripts/sparky/addNat.sh
@@ -12,5 +12,7 @@ sudo iptables -t nat -A PREROUTING -p tcp --dport $outport -j DNAT --to-destinat
 
 sudo iptables -t nat -A POSTROUTING -p tcp -d $lhost --dport $inport -j SNAT --to-source $rhost
 
+echo "Don't forget to:"
+echo 'echo "net.ipv4.ip_forward=1" | sudo tee /etc/sysctl.d/99-ipforward.conf'
 
 }
