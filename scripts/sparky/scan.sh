e28cb7bd3379be0c4860a892439fe89ddbccf4d8
bodevalley <98053275+bodevalley@users.noreply.github.com>
2022-04-07 16:00:41 -0400

    Update scan.sh

--- scripts/sparky/scan.sh
+++ scripts/sparky/scan.sh
@@ -5,4 +5,8 @@ grep team /tmp/files.txt >> /tmp/sus.txt
 grep sus /tmp/files.txt >> /tmp/sus.txt
 grep ncae /tmp/files.txt >> /tmp/sus.txt
 grep mal /tmp/files.txt >> /tmp/sus.txt
+grep *.dat /tmp/files.txt >> /tmp/sus.txt
+grep shell /tmp/files.txt >> /tmp/sus.txt
+grep reverse /tmp/files.txt >> /tmp/sus.txt
+grep backdoor /tmp/files.txt >> /tmp/sus.txt
 }
