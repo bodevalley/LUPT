d88768dd29f3a7364accbaec96bf1b0445a31bbd
bodevalley <98053275+bodevalley@users.noreply.github.com>
2022-04-08 00:24:02 -0400

    Update watcher.sh

--- scripts/watcher.sh
+++ scripts/watcher.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #opens two terminals that will watch the /etc and /home directories for accesses, changes, or deletions
-gnome-terminal -t ETC --geometry=100x200 -- inotifywait -m -r /etc -e access -modify -e moved_to -e delete >> log.etc
-gnome-terminal -t HOME --geometry=100x200 -- inotifywait -m -r /home -e access -modify -e moved_to -e delete >> log.home
+inotifywait -m  /etc/passwd -e access -modify -e moved_to -e delete >> log.etc
+inotifywait -m -r /home/ -e access -modify -e moved_to -e delete >> log.home
