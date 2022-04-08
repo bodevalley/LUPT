8a81053bfbd806dafaeee393156a48bbeefe98a5
bodevalley <98053275+bodevalley@users.noreply.github.com>
2022-04-07 23:36:31 -0400

    Update staticKali.sh

--- scripts/sparky/staticKali.sh
+++ scripts/sparky/staticKali.sh
@@ -3,14 +3,16 @@ echo "What is your IP?"
 read IP
 echo "What is your interface?"
 read interface
+echo "What is your gateway?"
+read gateway
 sudo cp /etc/network/interfaces /tmp/backup.static
 sudo rm /tmp/interfaces
 echo "auto lo" > /tmp/interfaces
 echo "iface lo inet loopback" >> /tmp/interfaces
 echo "auto "$interface >> /tmp/interfaces
 echo "iface "$interface" inet static" >> /tmp/interfaces
-echo -e "\taddress "$IP >> /tmp/interfaces
-echo -e "\tnetmask 255.255.255.0" >> /tmp/interfaces
+echo -e "\taddress "$IP"/24" >> /tmp/interfaces
+echo -e "\tgateway "$gateway >> /tmp/interfaces
 sudo chown root:root /tmp/interfaces
 sudo chmod 644 /tmp/interfaces
 sudo rm /etc/network/interfaces
