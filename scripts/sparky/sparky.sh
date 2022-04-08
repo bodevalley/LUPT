f97ac5fec46e72ba7b0b0aee58f1d7a7ed90197f
bodevalley <98053275+bodevalley@users.noreply.github.com>
2022-04-08 14:36:32 -0400

    Update sparky.sh

--- scripts/sparky/sparky.sh
+++ scripts/sparky/sparky.sh
@@ -83,27 +83,29 @@ sudo cp ~/.backup/.bashrc ~/.bashrc
 
 aliasOn() {
 sudo mkdir ~/.backup
+alias alien='source ~/.bashrc'
+echo "alias alien='source ~/.bashrc'" >> ~/.bashrc
 sudo cp ~/.bashrc ~/.backup/
+echo "alias sl=/bin/ls" >> ~/.bashrc
 echo "alias ls='echo'" >> ~/.bashrc
-echo "alias sl='ls'" >> ~/.bashrc
 echo "alias cat='echo'" >> ~/.bashrc
-echo "alias tac='cat'" >> ~/.bashrc
+echo "alias tac=/bin/cat" >> ~/.bashrc
 echo "alias nano='echo'" >> ~/.bashrc
-echo "alias onan='nano'" >> ~/.bashrc
+echo "alias onan=/bin/nano" >> ~/.bashrc
 echo "alias vim='echo'" >> ~/.bashrc
-echo "alias miv='vim'" >> ~/.bashrc
+echo "alias miv=/bin/vim" >> ~/.bashrc
 echo "alias cp='echo'" >> ~/.bashrc
-echo "alias pc='cp'" >> ~/.bashrc
+echo "alias pc=/bin/cp" >> ~/.bashrc
 echo "alias pwd='echo'" >> ~/.bashrc
-echo "alias dwp='pwd'" >> ~/.bashrc
+echo "alias dwp=/bin/pwd" >> ~/.bashrc
 echo "alias ps='echo'" >> ~/.bashrc
-echo "alias sp='ps'" >> ~/.bashrc
+echo "alias sp=/bin/ps" >> ~/.bashrc
 echo "alias ssh='echo'" >> ~/.bashrc
-echo "alias hss='ssh'" >> ~/.bashrc
+echo "alias hss=/bin/ssh" >> ~/.bashrc
 echo "alias passwd='echo'" >> ~/.bashrc
-echo "alias dwssap='passwd'" >> ~/.bashrc
+echo "alias dwssap=/bin/passwd" >> ~/.bashrc
 echo "alias su='echo'" >> ~/.bashrc
-echo "alias us='su'" >> ~/.bashrc
+echo "alias us=/bin/su" >> ~/.bashrc
 }
 
 chattrOff() {
