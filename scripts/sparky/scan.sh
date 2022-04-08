bdfd647a5906f853a013c80928009d38540b1105
bodevalley <98053275+bodevalley@users.noreply.github.com>
2022-04-08 00:02:15 -0400

    Update scan.sh

--- scripts/sparky/scan.sh
+++ scripts/sparky/scan.sh
@@ -1,10 +1,10 @@
 Scan() {
 sudo ls -laR / > /tmp/files.txt
-grep red /tmp/files.txt > /tmp/sus.txt
-grep team /tmp/files.txt >> /tmp/sus.txt
-grep sus /tmp/files.txt >> /tmp/sus.txt
+grep redteam /tmp/files.txt > /tmp/sus.txt
+grep suspicious /tmp/files.txt >> /tmp/sus.txt
 grep ncae /tmp/files.txt >> /tmp/sus.txt
-grep mal /tmp/files.txt >> /tmp/sus.txt
+grep malware /tmp/files.txt >> /tmp/sus.txt
+grep malcious /tmp/files.txt >> /tmp/sus.txt
 grep *.dat /tmp/files.txt >> /tmp/sus.txt
 grep shell /tmp/files.txt >> /tmp/sus.txt
 grep reverse /tmp/files.txt >> /tmp/sus.txt
