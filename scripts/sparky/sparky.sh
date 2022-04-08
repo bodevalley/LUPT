3f5ec1271ed6b5629d6a8d7312d0543ca7503381
bodevalley <98053275+bodevalley@users.noreply.github.com>
2022-04-07 23:45:37 -0400

    Update sparky.sh

--- scripts/sparky/sparky.sh
+++ scripts/sparky/sparky.sh
@@ -158,16 +158,17 @@ sudo chown root:root /etc/gshadow
 
 Scan() {
 sudo ls -laR / > /tmp/files.txt
-grep red /tmp/files.txt > /tmp/sus.txt
-grep team /tmp/files.txt >> /tmp/sus.txt
-grep sus /tmp/files.txt >> /tmp/sus.txt
+grep redteam /tmp/files.txt > /tmp/sus.txt
+grep suspicious /tmp/files.txt >> /tmp/sus.txt
 grep ncae /tmp/files.txt >> /tmp/sus.txt
-grep mal /tmp/files.txt >> /tmp/sus.txt
+grep malware /tmp/files.txt >> /tmp/sus.txt
+grep malcious /tmp/files.txt >> /tmp/sus.txt
 grep *.dat /tmp/files.txt >> /tmp/sus.txt
 grep shell /tmp/files.txt >> /tmp/sus.txt
 grep reverse /tmp/files.txt >> /tmp/sus.txt
 grep backdoor /tmp/files.txt >> /tmp/sus.txt
 }
+
 
 StaticCentOS() {
 echo "What is your IP?"
