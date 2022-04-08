38f078ab54390dab23cd6dbca99b6fcf66f76cd8
bodevalley <98053275+bodevalley@users.noreply.github.com>
2022-04-07 23:36:51 -0400

    Update staticUbuntu.sh

--- scripts/sparky/staticUbuntu.sh
+++ scripts/sparky/staticUbuntu.sh
@@ -1,9 +1,10 @@
-
 StaticUbuntu() {
 echo "What is your IP?"
 read IP
 echo "What is your interface?"
 read interface
+echo "What is your gateway?"
+read gateway
 sudo rm /tmp/01-network-manager-all.yaml
 echo "network:" > /tmp/01-network-manager-all.yaml
 echo -e "  version: 2" >> /tmp/01-network-manager-all.yaml
@@ -12,7 +13,7 @@ echo -e "  ethernets:" >> /tmp/01-network-manager-all.yaml
 echo -e "    "$interface":" >> /tmp/01-network-manager-all.yaml
 echo -e "      addresses:" >> /tmp/01-network-manager-all.yaml
 echo -e "      - "$IP"/24" >> /tmp/01-network-manager-all.yaml
-echo -e "      gateway4: 192.168.2.1" >> /tmp/01-network-manager-all.yaml
+echo -e "      gateway4: "$gateway >> /tmp/01-network-manager-all.yaml
 sudo chown root:root /tmp/01-network-manager-all.yaml 
 sudo chmod 644 /tmp/01-network-manager-all.yaml 
 sudo cp /etc/netplan/* /tmp/backup.netplan
@@ -20,3 +21,4 @@ sudo rm /etc/netplan/*
 sudo cp /tmp/01-network-manager-all.yaml  /etc/netplan/ 
 sudo netplan apply
 }
+
