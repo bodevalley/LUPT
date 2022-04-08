c3945b60005beaa9ddb14382cda8cc9bb6bd166a
bodevalley <98053275+bodevalley@users.noreply.github.com>
2022-04-08 00:01:50 -0400

    Update backupCron.sh

--- scripts/sparky/backupCron.sh
+++ scripts/sparky/backupCron.sh
@@ -7,12 +7,12 @@ sudo mkdir ~/.backupCron/mysql
 sudo mkdir ~/.backupCron/ftp
 sudo mkdir ~/.backupCron/important
 
-(crontab -l ; echo "5 * * * * cp -f /etc/apache2/* ~/.backupCron/apache/") | crontab -
-(crontab -l ; echo "5 * * * * cp -f /etc/bind/* ~/.backupCron/bind/") | crontab -
-(crontab -l ; echo "5 * * * * cp -f /etc/mysql/* ~/.backupCron/mysql/") | crontab -
-(crontab -l ; echo "5 * * * * cp -f /etc/vsftpd/* ~/.backupCron/ftp/") | crontab -
-(crontab -l ; echo "5 * * * * cp -f /etc/passwd ~/.backupCron/important/") | crontab -
-(crontab -l ; echo "5 * * * * cp -f /etc/network/interfaces ~/.backupCron/important/") | crontab -
-(crontab -l ; echo "5 * * * * cp -f /etc/netplan/* ~/.backupCron/important/") | crontab -
+(crontab -l ; echo "15 * * * * cp -f /etc/apache2/* ~/.backupCron/apache/") | crontab -
+(crontab -l ; echo "15 * * * * cp -f /etc/bind/* ~/.backupCron/bind/") | crontab -
+(crontab -l ; echo "15 * * * * cp -f /etc/mysql/* ~/.backupCron/mysql/") | crontab -
+(crontab -l ; echo "15 * * * * cp -f /etc/vsftpd/* ~/.backupCron/ftp/") | crontab -
+(crontab -l ; echo "15 * * * * cp -f /etc/passwd ~/.backupCron/important/") | crontab -
+(crontab -l ; echo "15 * * * * cp -f /etc/network/interfaces ~/.backupCron/important/") | crontab -
+(crontab -l ; echo "15 * * * * cp -f /etc/netplan/* ~/.backupCron/important/") | crontab -
 
 }
